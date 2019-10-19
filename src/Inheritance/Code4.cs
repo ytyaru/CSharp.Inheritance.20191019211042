fffa24836edcfa8de85e152c00698b2519deda77
ytyaru <ytyaru@outlook.jp>
2019-10-19 21:11:10 +0900

    初回コミット

---
+++ src/Inheritance/Code4.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection;
+
+namespace Inheritance
+{
+    class Code4
+    {
+        public void Run()
+        {
+            Type t = typeof(SimpleClass);
+            BindingFlags flags = BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public |
+                                 BindingFlags.NonPublic | BindingFlags.FlattenHierarchy;
+            MemberInfo[] members = t.GetMembers(flags);
+            Console.WriteLine($"Type {t.Name} has {members.Length} members: ");
+            foreach (var member in members)
+            {
+                string access = "";
+                string stat = "";
+                var method = member as MethodBase;
+                if (method != null)
+                {
+                    if (method.IsPublic)
+                        access = " Public";
+                    else if (method.IsPrivate)
+                        access = " Private";
+                    else if (method.IsFamily)
+                        access = " Protected";
+                    else if (method.IsAssembly)
+                        access = " Internal";
+                    else if (method.IsFamilyOrAssembly)
+                        access = " Protected Internal ";
+                    if (method.IsStatic)
+                        stat = " Static";
+                }
+                var output = $"{member.Name} ({member.MemberType}): {access}{stat}, Declared by {member.DeclaringType}";
+                Console.WriteLine(output);
+            }
+
+            SimpleClass sc = new SimpleClass();
+            Console.WriteLine(sc.ToString());
+        }
+    }
+    public class SimpleClass
+    { }
+}
---
+++ src/Inheritance/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//            new Code0().Run();
+//            new Code1().Run();
+            new Code4().Run();
+        }
+    }
+}
